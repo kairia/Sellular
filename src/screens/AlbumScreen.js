fb28a3d97b9009f43f33485b8d56009cd680cd67
kairia <8695024+kairia@users.noreply.github.com>
2022-06-03 23:22:53 +0800

    AddedFirebase

--- src/screens/AlbumScreen.js
+++ src/screens/AlbumScreen.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import { Box,useColorMode } from "native-base";
-import { ImageBackground,Text,Platform,View,Image } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { Box,Progress,useColorMode } from "native-base";
+import { ImageBackground,Text,Platform,View,Image,Animated,StyleSheet,Dimensions } from "react-native";
 import AlbumList from "../components/AlbumList";
 import albumData from "../json/albums.json";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+
 import { useFonts } from 'expo-font';
+
+const { width, height } = Dimensions.get("screen");
 
 var headerBgColor =["#fff","#000" ];
 var headerTitleColor=["#121212",'#8281D7'];
@@ -17,7 +20,12 @@ var lg=[
   require("../images/Linear_Gradient_DB.png"),
 
 ]
+
+
+
+
 const AlbumScreen = ({ navigation }) => {
+
   const { colorMode } = useColorMode();
   state = {
     fontsLoaded: false,
@@ -35,13 +43,13 @@ const AlbumScreen = ({ navigation }) => {
 
 
   return (
-    Platform.OS == 'ios' ?
-    <><Box bg={colorMode == "light"?headerBgColor[0]:headerBgColor[1]} width="100%" height="15%" style={{shadowColor: '#171717',shadowOffset:{width: -2, height: 4},shadowOpacity: 0.2,shadowRadius: 3,}}>
+    <>
+    <Box bg={colorMode == "light"?headerBgColor[0]:headerBgColor[1]} width="100%" height="12%" style={{shadowColor: '#171717',shadowOffset:{width: -2, height: 4},shadowOpacity: 0.2,shadowRadius: 3,justifyContent:'center',flexDirection:'column'}}>
       <Text
         style={{
           fontFamily: fontsLoaded ? 'Contrail One' : 'Roboto',
           fontSize: 48,
-          top: "40%",
+          top: "28%",
           left: "7%",
           color:colorMode == "light"?headerTitleColor[0]:headerTitleColor[1],
         }}>Home</Text>
@@ -49,7 +57,7 @@ const AlbumScreen = ({ navigation }) => {
         name={'menu'}
         size={24}
         onPress={() => navigation.openDrawer()}
-        style={{ marginLeft: "87%" }}
+        style={{ marginLeft: "86%",bottom:'35%' }}
         color={colorMode == "light"?'#121212':'#fff'} />
     </Box>
     <View backgroundColor={"#F3FAFF"} >
@@ -58,16 +66,36 @@ const AlbumScreen = ({ navigation }) => {
           list={albumData.albumList}
           navigation={navigation} />
 
-      </View></>:
-      <><Box backgroundColor={"#F3FAFF"}>
-        <AlbumList
-          style={{ width: "100%" }}
-          list={albumData.albumList}
-          navigation={navigation} />
+      </View>
+      <View style={styles.square}>
 
-      </Box><Image source={colorMode == "light"?lg[0]:lg[1]} style={{ position: 'absolute',bottom:0,width:"100%" }}></Image></>
+      </View>
+      </>
 
   );
 };
+
+
+const styles = StyleSheet.create({
+    
+  square:{
+    position:'absolute',
+    alignSelf:'center',
+    bottom:height*0.5-100,
+    width:width*0.2,
+    height:height*0.2,
+    backgroundColor:'#3E5EB0',
+
+  }
+
+
+
+
+
+
+
+
+})
+
 
 export default AlbumScreen;
